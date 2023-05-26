c3c68804990241550841822c0dfcf56777be253c
IdoyaGE <igearkabaia@gmail.com>
2023-05-26 14:46:36 +0200

    commit conserva los mensajes escritos en el chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 import "../styles/Chat.css";
 
 export const Chat = (props) => {
   const { room } = props;
   const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const messagesRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (newMessage === "") return;
@@ -19,7 +38,13 @@ export const Chat = (props) => {
     setNewMessage("");
   };
   return (
-    <div className='chat.app'>
+    <div className='chat-app'>
+      <div>
+        {" "}
+        {messages.map((message) => (
+          <h1> {message.text}</h1>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className='new-message-form'>
         <input
           className='new-message-input'
