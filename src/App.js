8202324863a69e78790865f4feb1a1b921f328d1
IdoyaGE <igearkabaia@gmail.com>
2023-05-28 23:26:01 +0200

    Commit consejos con imágenes y estilo scss

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ import room2 from "./images/juegos.png";
 import room3 from "./images/seguridad.png";
 import room4 from "./images/postparto.png";
 import room5 from "./images/productos.png";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Footer from "./components/Footer";
 import Blog from "./components/Blog";
-
 import momplanet from "./images/momplanet.png";
+import depresion from "./images/depresion.png";
+import dieta from "./images/dieta.png";
+import ejercicio from "./images/ejercicio.png";
 
 const cookies = new Cookies();
 
@@ -102,6 +104,53 @@ function App() {
         <button onClick={signUserOut}>Salir</button>
       </div>
 
+      <h2>Consejos</h2>
+      <div className='consejos'>
+        <ul className='columns'>
+          <li className='box'>
+            <img
+              src={depresion}
+              id='iconos'
+              alt='Logo informe'
+              width='150px'
+              height='150px'
+            />
+            <h1 className='tituloconsejos'>Estado emocional</h1>
+            <p>
+              La lactancia, el cansancio de las primeras semanas, las noches en
+              vela. Pon atención a tu estado anímico.
+            </p>
+          </li>
+          <li className='box'>
+            <img
+              src={dieta}
+              id='iconos'
+              alt='Logo hucha'
+              width='150px'
+              height='150px'
+            />
+            <h1 className='tituloconsejos'>Alimentación</h1>
+            <p>
+              Mantener una alimentación equilibrada y una correcta hidratación
+              nos ayudará a tener más energía.
+            </p>
+          </li>
+          <li className='box'>
+            <img
+              src={ejercicio}
+              id='iconos'
+              alt='Logo pieza puzzle'
+              width='150px'
+              height='150px'
+            />
+            <h1 className='tituloconsejos'>Ejercicio físico</h1>
+            <p>
+              Hacer deporte no sólo nos ayudará a recuperar nuestro peso y
+              sentirnos mejor físicamente.
+            </p>
+          </li>
+        </ul>
+      </div>
       <Blog />
       <Footer />
     </div>
